dd485b7cbb581fb6bca3f783d989c4997517811f
dahromy <romeorazaf@gmail.com>
2022-02-03 08:59:57 +0300

    Github connect

---
+++ src/Repository/UserRepository.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\User;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\Persistence\ManagerRegistry;
+use League\OAuth2\Client\Provider\GithubResourceOwner;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @method User|null find($id, $lockMode = null, $lockVersion = null)
+ * @method User|null findOneBy(array $criteria, array $orderBy = null)
+ * @method User[]    findAll()
+ * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class UserRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, User::class);
+    }
+
+    /**
+     * Used to upgrade (rehash) the user's password automatically over time.
+     */
+    public function upgradePassword(UserInterface $user, string $newHashedPassword): void
+    {
+        if (!$user instanceof User) {
+            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', \get_class($user)));
+        }
+
+        $user->setPassword($newHashedPassword);
+        $this->_em->persist($user);
+        $this->_em->flush();
+    }
+
+    /**
+     * @param GithubResourceOwner $owner
+     * @return User
+     * @throws NonUniqueResultException
+     */
+    public function findOrCreateFromGithubOauth(GithubResourceOwner $owner): User
+    {
+        $em = $this->getEntityManager();
+
+        /** @var User|null $user */
+        $user = $this->createQueryBuilder('u')
+            ->where('u.githubId = :githubId')
+            ->orWhere('u.email = :email')
+            ->setParameters([
+                'githubId' => $owner->getId(),
+                'email' => $owner->getEmail()
+            ])
+            ->getQuery()
+            ->getOneOrNullResult();
+
+        if ($user){
+
+            if ($user->getGithubId() === null){
+                $user->setGithubId($owner->getId());
+
+                $em->persist($user);
+                $em->flush();
+            }
+
+            return $user;
+        }
+
+        $user = (new User())
+            ->setRoles(['ROLE_USER'])
+            ->setGithubId($owner->getId())
+            ->setEmail($owner->getEmail());
+
+        $em->persist($user);
+        $em->flush();
+
+        return $user;
+    }
+}
